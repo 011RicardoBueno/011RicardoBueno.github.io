da05e8801a42b93d2761ff32fa2fe9afb5f88c3d
011RicardoBueno <therbueno@gmail.com>
2020-10-13 17:54:49 -0300

    form update

--- src/components/contactUs.js
+++ src/components/contactUs.js
@@ -10,11 +10,11 @@ const ContactUs = () => {
     <div className='wrapper'>
       <div className='contact-form' id='form'>
         <div className='input-fields'>
-        <form method='post' netlify netlify-honeypot='bot-field' name='contato' autoComplete='off'>
+        <form method='post' netlify-honeypot='bot-field' name='contato' autoComplete='off' netlify>
 
         <input type="text" className='input' required="required" name="name" placeholder='Nome'/>
         <input type="tel" className='input' required="required" maxlength="15" name="telefone" placeholder='(00) 0000-0000' />
-        <input type="email" className='input' required="required" class="input-text" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" placeholder='E-mail'/>
+        <input type="email" className='input' required="required" class="input-text" name="email" placeholder='E-mail'/>
         <input type='text' className='input' name='subject' id='subject' placeholder='Assunto' />
         <input type="hidden" name="bot-field" />
 
