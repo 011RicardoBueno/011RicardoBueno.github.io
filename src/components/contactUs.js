b0762fab187cbbe52b5dbaa3a22c8a146f0ff49a
011RicardoBueno <therbueno@gmail.com>
2020-10-13 20:26:06 -0300

    form again

--- src/components/contactUs.js
+++ src/components/contactUs.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
 const ContactUs = () => {
 
-  const { register, handleSubmit, errors } = useForm();
-  const onSubmit = data => {
-    fetch('/thanks', {
-    method: 'POST',
-    body: data,
+  const { register, handleSubmit, reset } = useForm();
+  const [state, setState] = React.useState({});
+  const [feedbackMsg, setFeedbackMsg] = useState(null);
+  const handleChange = e => setState({ ...state, [e.target.name]: e.target.value })
+  const onSubmit = (data, e) => {
+    e.preventDefault()
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        // 'form-name': form.getAttribute('name'),
+        'form-name': 'contato',
+        ...state,
+      }),
     })
-    .then(data => console.log(data))
-    .catch(error => alert(error));
-    };
+      .then(response => {
+        setFeedbackMsg(`Thanks for reaching out. I'll get back to you soon.`)
+        reset()
+        console.log(response)
+      })
+      .catch(error => {
+        setFeedbackMsg("Oops, something went wrong. The form could not be submitted.")
+        console.log(error)
+      })
+  };
   return(
     <section id='form'>
     <div className='wrapper'>
@@ -20,7 +42,7 @@ const ContactUs = () => {
         <div className='input-fields'>
         <form onSubmit={handleSubmit(onSubmit)} netlify-honeypot='bot-field' name='contato' autoComplete='off' data-netlify="true">
 
-        <input type="text" className='input' required="required" name="name" placeholder='Nome' ref={register()} />
+        <input type="text" className='input' required="required" name="name" placeholder='Nome' onChange={handleChange} ref={register()} />
         <input type="tel" className='input' required="required" name="telefone" placeholder='(00) 0000-0000' ref={register()}/>
         <input type="email" className='input' required="required" name="email" placeholder='E-mail' ref={register({
           required: "Enter your e-mail",
@@ -29,7 +51,7 @@ const ContactUs = () => {
             message: "Digite um e-mail válido",
           },
          })}/>
-         {errors.email && <p className="error">{errors.email.message}</p>}
+         {feedbackMsg && <p className="success">{feedbackMsg}</p>}
         <input type='text' className='input' name='subject' id='subject' placeholder='Assunto' ref={register()}/>
         <input type="hidden" name="bot-field" />
 
