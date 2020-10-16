c2f8c543d36d64c8b03db446c30805e7e4cb6793
011RicardoBueno <therbueno@gmail.com>
2020-10-16 00:38:59 -0300

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,31 @@ module.exports = {
       options: {
         username: `25341904896`,
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-151639095-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+         // Enables Google Optimize Experiment ID
+        experimentId: "16iQisXuS1qwXDixwB-EWgQ",
+          // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "zero11web.netlify.app",
+      },
     }
   ],
 }
